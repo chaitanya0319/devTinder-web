c3bba32e530fb2e0e946b444493a686b13ed2540
Chaitanya <Chaitanyabandi18@gmail.com>
2025-12-13 15:51:58 -0600

    redirect to login if not autehticated

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <div className="navbar bg-base-300">
     <div className="flex-1">
-      <a className="btn btn-ghost text-xl">👩‍💻 DevTinder</a>
+    <Link to="/" className="btn btn-ghost text-xl">
+          👩‍💻 DevTinder
+        </Link>
     </div>
     <div className="flex-none gap-2">
       <div className="form-control"></div>
@@ -26,10 +29,10 @@ const NavBar = () => {
           className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
         >
           <li>
-            <a className="justify-between">
+          <Link to="/profile" className="justify-between">
               Profile
               <span className="badge">New</span>
-            </a>
+            </Link>
           </li>
           <li>
             <a>Settings</a>
