eb440af4245f9f94f7d79192575417e6a0087c3f
Chaitanya <Chaitanyabandi18@gmail.com>
2025-12-14 16:42:30 -0600

    Typo error

--- src/utils/appStore.js
+++ src/utils/appStore.js
@@ -2,7 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
 import feedReducer from "./feedSlice";
 import connectionReducer from "./conectionSlice";
-import requestReducer from "./";
+import requestReducer from "./requestSlice";
 
 const appStore = configureStore({
   reducer: {
